a22e775f9345689770304a8ed0db8a4a4e2fbfcb
Sebastian <sebastiancarmona@veryfi.com>
2022-04-07 18:42:41 -0500

    Update docs.

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"veryfi"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"veryfi"},{"l":"veryfi.models"}];updateSearchResults();
